ebcb2ed778a6ae1add997a9830ee259e59101c2b
Frank <frank_robbenok@hotmail.com>
2016-05-21 11:53:23 +0900

    I have integrated saving screenshots code into our service

--- ScreenshotOCR/app/src/main/java/net/rahar/screenshotocr/ScreenOCR.java
+++ ScreenshotOCR/app/src/main/java/net/rahar/screenshotocr/ScreenOCR.java
@@ -13,13 +13,26 @@ import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
 import android.media.ExifInterface;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.FileObserver;
+import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -35,9 +48,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 public class ScreenOCR extends Activity {
-    String TAG = "ScreenOCRMainActivity";
+    //String TAG1 = "ScreenOCRMainActivity";
     String LANG = "eng";
     public static String DATA_DIR_PATH;
     String TESS_DATA_DIR;
@@ -47,6 +61,123 @@ public class ScreenOCR extends Activity {
     public static ImageView image;
     public static final String ACTION = "com.test.www.Action";
     MyReceiver mReceiver;
+
+
+    private static final String TAG = ScreenOCR.class.getName();
+    private static final int REQUEST_CODE = 100;
+    private static String STORE_DIRECTORY;
+    private static int IMAGES_PRODUCED;
+    private static final String SCREENCAP_NAME = "screencap";
+    private static final int VIRTUAL_DISPLAY_FLAGS = DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY | DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC;
+    private static MediaProjection sMediaProjection;
+
+    private MediaProjectionManager mProjectionManager;
+    private ImageReader mImageReader;
+    private Handler mHandler;
+    private Display mDisplay;
+    private VirtualDisplay mVirtualDisplay;
+    private int mDensity;
+    private int mWidth;
+    private int mHeight;
+    private int mRotation;
+    private OrientationChangeCallback mOrientationChangeCallback;
+
+    public File storeDirectory;
+
+
+    private class ImageAvailableListener implements ImageReader.OnImageAvailableListener {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image image = null;
+            FileOutputStream fos = null;
+            Bitmap bitmap = null;
+
+            try {
+                image = mImageReader.acquireLatestImage();
+                if (image != null) {
+                    Image.Plane[] planes = image.getPlanes();
+                    ByteBuffer buffer = planes[0].getBuffer();
+                    int pixelStride = planes[0].getPixelStride();
+                    int rowStride = planes[0].getRowStride();
+                    int rowPadding = rowStride - pixelStride * mWidth;
+
+                    // create bitmap
+                    bitmap = Bitmap.createBitmap(mWidth + rowPadding / pixelStride, mHeight, Bitmap.Config.ARGB_8888);
+                    bitmap.copyPixelsFromBuffer(buffer);
+
+                    // write bitmap to a file
+                    File file= new File(storeDirectory, "myscreen_" + IMAGES_PRODUCED + ".png");
+                    fos = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+
+                    IMAGES_PRODUCED++;
+                    Log.e(TAG, "captured image: " + IMAGES_PRODUCED);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (fos!=null) {
+                    try {
+                        fos.close();
+                    } catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    }
+                }
+
+                if (bitmap!=null) {
+                    bitmap.recycle();
+                }
+
+                if (image!=null) {
+                    image.close();
+                }
+            }
+        }
+    }
+
+    private class OrientationChangeCallback extends OrientationEventListener {
+        public OrientationChangeCallback(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            synchronized (this) {
+                final int rotation = mDisplay.getRotation();
+                if (rotation != mRotation) {
+                    mRotation = rotation;
+                    try {
+                        // clean up
+                        if(mVirtualDisplay != null) mVirtualDisplay.release();
+                        if(mImageReader != null) mImageReader.setOnImageAvailableListener(null, null);
+
+                        // re-create virtual display depending on device width / height
+                        createVirtualDisplay();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private class MediaProjectionStopCallback extends MediaProjection.Callback {
+        @Override
+        public void onStop() {
+            Log.e("ScreenCapture", "stopping projection.");
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if(mVirtualDisplay != null) mVirtualDisplay.release();
+                    if(mImageReader != null) mImageReader.setOnImageAvailableListener(null, null);
+                    if(mOrientationChangeCallback != null) mOrientationChangeCallback.disable();
+                    sMediaProjection.unregisterCallback(MediaProjectionStopCallback.this);
+                }
+            });
+        }
+    }
+
 
     /**
      * Tesseract OCR works with a training data file that must be located at the file system. I'm shipping the training data
@@ -87,6 +218,8 @@ public class ScreenOCR extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_ocr);
 
+        mProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
         btn=(Button)findViewById(R.id.button);
 
         DATA_DIR_PATH = getApplicationContext().getExternalFilesDir(null).getAbsolutePath();
@@ -125,11 +258,21 @@ public class ScreenOCR extends Activity {
                 clipboard.setPrimaryClip(clip);
             }
         });
+
+        new Thread() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                mHandler = new Handler();
+                Looper.loop();
+            }
+        }.start();
     }
 
 
     public void mOnClick(View v){
         if (flag==0){
+            startProjection();
             btn.setText("Stop Service");
             flag=1;
             mReceiver=new MyReceiver();
@@ -138,6 +281,7 @@ public class ScreenOCR extends Activity {
             sendBroadcast(i);
         }
         else{
+            stopProjection();
             btn.setText("Start Service");
             flag=0;
         }
@@ -158,4 +302,83 @@ public class ScreenOCR extends Activity {
     //    unregisterReceiver(mReceiver);
         super.onPause();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_CODE) {
+
+            sMediaProjection = mProjectionManager.getMediaProjection(resultCode, data);
+
+            if (sMediaProjection != null) {
+
+                File externalFilesDir = getExternalFilesDir(null);
+                if (externalFilesDir != null) {
+
+                    STORE_DIRECTORY = Environment.getExternalStorageDirectory() + "/Pictures/Screenshots";
+                    Toast.makeText(this, STORE_DIRECTORY, Toast.LENGTH_LONG).show();
+                    storeDirectory = new File(STORE_DIRECTORY);
+                    if (!storeDirectory.exists()) {
+
+                        boolean success = storeDirectory.mkdirs();
+                        if (!success) {
+                            Log.e(TAG, "failed to create file storage directory.");
+                            return;
+                        }
+                    }
+                } else {
+                    Log.e(TAG, "failed to create file storage directory, getExternalFilesDir is null.");
+                    return;
+                }
+
+                // display metrics
+                DisplayMetrics metrics = getResources().getDisplayMetrics();
+                mDensity = metrics.densityDpi;
+                mDisplay = getWindowManager().getDefaultDisplay();
+
+                // create virtual display depending on device width / height
+                createVirtualDisplay();
+
+                // register orientation change callback
+                mOrientationChangeCallback = new OrientationChangeCallback(this);
+                if (mOrientationChangeCallback.canDetectOrientation()) {
+                    mOrientationChangeCallback.enable();
+                }
+
+                // register media projection stop callback
+                sMediaProjection.registerCallback(new MediaProjectionStopCallback(), mHandler);
+            }
+        }
+    }
+
+    /****************************************** UI Widget Callbacks *******************************/
+    private void startProjection() {
+        startActivityForResult(mProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
+    }
+
+    private void stopProjection() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (sMediaProjection != null) {
+                    sMediaProjection.stop();
+                }
+            }
+        });
+    }
+
+    /****************************************** Factoring Virtual Display creation ****************/
+    private void createVirtualDisplay() {
+        // get width and height
+        Point size = new Point();
+        mDisplay.getSize(size);
+        mWidth = size.x;
+        mHeight = size.y;
+
+        // start capture reader
+        mImageReader = ImageReader.newInstance(mWidth, mHeight, PixelFormat.RGBA_8888, 2);
+        mVirtualDisplay = sMediaProjection.createVirtualDisplay(SCREENCAP_NAME, mWidth, mHeight, mDensity, VIRTUAL_DISPLAY_FLAGS, mImageReader.getSurface(), null, mHandler);
+        mImageReader.setOnImageAvailableListener(new ImageAvailableListener(), mHandler);
+    }
+
 }
